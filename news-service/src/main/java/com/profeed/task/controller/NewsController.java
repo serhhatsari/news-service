45def33195516c20184cd9be6a59ba66e12e40b1
Serhat SARI <serhhatsari@gmail.com>
2023-02-02 17:34:02 +0300

    enabled cors

--- news-service/src/main/java/com/profeed/task/controller/NewsController.java
+++ news-service/src/main/java/com/profeed/task/controller/NewsController.java
@@ -19,7 +19,7 @@ public class NewsController {
     private final NewsService newsService;
 
     @GetMapping("/news")
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @CrossOrigin
     public ResponseEntity<List<NewsDto>> getNews(@RequestParam(value = "page", required = true) int page, @RequestParam(value = "size", required = true) int size, @RequestParam(value = "source", required = false) String source, @RequestParam(value = "publishedDateStart", required=false)  @DateTimeFormat(pattern="yyyy-MM-dd") Date publishedDateStart, @RequestParam(value = "publishedDateEnd", required=false) @DateTimeFormat(pattern="yyyy-MM-dd") Date publishedDateEnd, @RequestParam(value="titleContains", required=false) String titleContains, @RequestParam(value="country", required=false) String country, @RequestParam(value = "language", required=false) String language) {
         return new ResponseEntity<>(newsService.filterNews(page, size, source, publishedDateStart, publishedDateEnd, titleContains, country, language), HttpStatus.OK);
     }
