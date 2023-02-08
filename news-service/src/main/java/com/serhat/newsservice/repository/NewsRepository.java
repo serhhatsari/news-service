87d3d14246c59a01c0c2015f537227bcf3572529
Serhat SARI <serhhatsari@gmail.com>
2023-02-08 04:17:40 +0300

    implement new methods in service

--- news-service/src/main/java/com/serhat/newsservice/repository/NewsRepository.java
+++ news-service/src/main/java/com/serhat/newsservice/repository/NewsRepository.java
@@ -3,7 +3,16 @@ package com.serhat.newsservice.repository;
 import com.serhat.newsservice.model.entity.NewsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface NewsRepository extends JpaRepository<NewsEntity, Long> {
 
+    Optional<List<NewsEntity>> findByCategory(String category);
 
+    Optional<List<NewsEntity>> findByCountry(String country);
+
+    Optional<List<NewsEntity>> findByLanguage(String language);
+
+    Optional<List<NewsEntity>> findTop10ByOrderByPublished_atDesc();
 }
